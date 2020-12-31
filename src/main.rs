1af68abea353ed0717c7ae658aa325b648e77f20
Bee <pizzaman8099@gmail.com>
2020-12-31 15:36:54 -0800

    Complete implementation of main features
    All that is left now is to add the printing of the failed URLS to the compact output.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,33 @@ use url::{ParseError, Url};
 #[structopt(name = "Polymonitor")]
 /// Simple http response monitor built to be used with Polybar
 struct Opt {
-    /// Displays the results as symbols
-    #[structopt(short, long)]
-    symbolic: bool,
     /// Reduces the results into a more compact package
     #[structopt(short, long)]
     compact: bool,
+    /// Displays the results as symbols
+    #[structopt(short, long)]
+    symbolic: bool,
     /// Pass in URLS to monitor
     urls: Vec<String>,
+}
+
+struct ValMap<'a> {
+    up: &'a str,
+    down: &'a str,
+}
+
+fn make_value_map<'a>(symbolic: bool) -> ValMap<'a> {
+    if symbolic {
+        ValMap {
+            up: "\u{f062}",
+            down: "\u{f98d}",
+        }
+    } else {
+        ValMap {
+            up: "Up",
+            down: "Down",
+        }
+    }
 }
 
 fn parse_url(url: &String) -> String {
@@ -38,26 +57,44 @@ fn get_status(url: &str) -> bool {
     res.ok()
 }
 
-fn map_statuses(urls: Vec<String>) -> Vec<(String, bool)> {
-    urls.into_iter()
-        .map(|url| {
+fn main() -> Result<(), &'static str> {
+    let Opt {
+        compact,
+        symbolic,
+        urls,
+    } = Opt::from_args();
+
+    if urls.is_empty() {
+        return Err("Please pass in valid urls you would like to monitor");
+    } else {
+        let statuses = urls.iter().map(parse_url).map(|url| {
             let owned = url.to_owned();
             let stat = get_status(&owned);
             (owned, stat)
-        })
-        .collect()
-}
+        });
 
-fn main() -> Result<(), &'static str> {
-    let args = Opt::from_args();
+        let val_map = make_value_map(symbolic);
 
-    if args.urls.is_empty() {
-        return Err("Please pass in valid urls you would like to monitor");
-    } else {
-        let parsed_urls = args.urls.iter().map(parse_url).collect();
-        println!("{:?}", parsed_urls);
-        let status = map_statuses(parsed_urls);
-        println!("{:?}", status);
+        let parsed_output = if compact {
+            let count = statuses.fold((0, 0), |acc, cur| {
+                if cur.1 {
+                    (acc.0 + 1, acc.1)
+                } else {
+                    (acc.0, acc.1 + 1)
+                }
+            });
+            format!("{}: {}, {}: {}", val_map.up, count.0, val_map.down, count.1)
+        } else {
+            statuses
+                .map(|(url, status)| {
+                    let parsed_status = if status { val_map.up } else { val_map.down };
+                    format!("{}: {}", url, parsed_status).to_owned()
+                })
+                .collect::<Vec<String>>()
+                .join(", ")
+        };
+
+        println!("{}", parsed_output);
 
         Ok(())
     }
